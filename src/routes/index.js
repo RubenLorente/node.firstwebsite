8b0d59d6e5893082ac1a9e1f5f46bf5f57434391
RubenLorente <zgzrubenlorente@gmail.com>
2021-10-21 05:20:24 +0200

    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,22 +4,12 @@ const router = express.Router();
 
 router.get('/', (req, res) =>{
     //res.sendFile(path.join(__dirname, 'views/index.html'));
-    try{
-        res.render('index.html', { title: 'First website' });
-    }catch(error){
-        console.error(error);
-        return res.status(500).send("Server error")
-    }
+    res.render('index.html', { title: 'First website' });
 });
 
 router.get('/contact', (req, res) =>{
     //res.sendFile(path.join(__dirname, 'views/index.html'));
-    try{
-        res.render('contact.html', { title: 'Contact page' });
-    }catch(error){
-        console.error(error);
-        return res.status(500).send("Server error")
-    }
+    res.render('contact.html', { title: 'Contact page' });
 });
 
 module.exports = router;
