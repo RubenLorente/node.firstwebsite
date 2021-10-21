a2b02bbeb4f560db528e2590ec1fb8f52b8a76cf
RubenLorente <zgzrubenlorente@gmail.com>
2021-10-21 05:12:25 +0200

    ficheros modificados

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');  //modulo que detecta en que SO estas para las barras de las rutas
+const PORT = process.env.PORT || 3000;
 
 // settings
-app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -18,6 +18,6 @@ app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // listening server
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+app.listen(PORT, () => {
+    console.log('Server on port', PORT);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,22 @@ const router = express.Router();
 
 router.get('/', (req, res) =>{
     //res.sendFile(path.join(__dirname, 'views/index.html'));
-    res.render('index.html', { title: 'First website' });
+    try{
+        res.render('index.html', { title: 'First website' });
+    }catch(error){
+        console.error(error);
+        return res.status(500).send("Server error")
+    }
 });
 
 router.get('/contact', (req, res) =>{
-    res.render('contact.html', { title: 'Contact Page'});
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
+    try{
+        res.render('contact.html', { title: 'Contact page' });
+    }catch(error){
+        console.error(error);
+        return res.status(500).send("Server error")
+    }
 });
 
 module.exports = router;
